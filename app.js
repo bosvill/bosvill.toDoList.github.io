fb60f0596293ebc7b582c863b1c1a6f6f008c014
bosvill <sv.boecker@gmail.com>
2023-03-06 16:52:20 +0100

    second

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
 addToDoButton.addEventListener("click", function () {
-	var paragraph = document.createElement("p");
-	paragraph.classList.add("paragraph-styling");
-	paragraph.innerText = inputField.value;
-	toDoContainer.appendChild(paragraph);
+	let todoItem = document.createElement("li");
+	todoItem.classList.add("paragraph-styling");
+	todoItem.innerText = inputField.value;
+	toDoContainer.appendChild(todoItem);
 	inputField.value = "";
-	paragraph.addEventListener("click", function () {
-		paragraph.style.textDecoration = "line-through";
+	todoItem.addEventListener("click", function () {
+		todoItem.style.textDecoration = "line-through";
 	});
-	paragraph.addEventListener("dblclick", function () {
-		toDoContainer.removeChild(paragraph);
+	todoItem.addEventListener("dblclick", function () {
+		toDoContainer.removeChild(todoItem);
 	});
 });
