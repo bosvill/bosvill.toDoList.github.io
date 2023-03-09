bb472db21c51dff7788c54c7ce2c8138e2d71448
bosvill <sv.boecker@gmail.com>
2023-03-09 16:30:32 +0100

    add edit

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 // 1. Create variables
+const todoContainer = document.querySelector('.to-dos')
+const todoItem = document.querySelectorAll('.todo-item')
+const inputField = document.querySelector('#inputField')
 
-let addToDoButton = document.getElementById("addToDo");
-let toDoContainer = document.getElementById("toDoContainer");
-let inputField = document.getElementById("inputField");
 
-// 2. Add eventListener for submit button
+const add = () => {
+	let todoItem = document.createElement('li')
+	todoItem.classList.add('todo-item')
+	todoContainer.appendChild(todoItem)
 
-addToDoButton.addEventListener("click", () => {
-	// Add li to ul
-	let todoItem = document.createElement("li");
-	todoItem.classList.add("todo-item");
-	toDoContainer.appendChild(todoItem);
-	// Add p for text from input
-	let todoText = document.createElement("p");
-	todoText.classList.add("todo-text");
-	todoText.innerText = inputField.value;
-	todoItem.appendChild(todoText);
-	// Add done button with 2 classes, .ready for styling
-	let doneButton = document.createElement("button");
-	doneButton.classList.add("btn", "done");
-	doneButton.innerText = "Done";
-	todoItem.appendChild(doneButton);
-	doneButton.addEventListener("click", () => {
-		todoText.classList.add("ready");
-	});
-	// Add edit button with 2 classes
-	let editButton = document.createElement("button");
-	editButton.classList.add("btn", "edit");
-	editButton.innerText = "Edit";
-	todoItem.appendChild(editButton);
+	let todoText = document.createElement('p')
+	todoText.classList.add('todo-text')
+	todoText.innerText = inputField.value
+	todoItem.appendChild(todoText)
 
-	// Missing eventListener and class for editButton
+	let doneButton = document.createElement('button')
+	doneButton.classList.add('btn', 'done')
+	doneButton.innerText = 'Done'
+	todoItem.appendChild(doneButton)
 
-	// Add delete button with 2 classes
-	let deleteButton = document.createElement("button");
-	deleteButton.classList.add("btn", "delete");
-	deleteButton.innerText = "Delete";
-	todoItem.appendChild(deleteButton);
-	// delete li with all children
-	deleteButton.addEventListener("click", () => {
-		toDoContainer.removeChild(todoItem);
-	});
-	// Clear input
-	inputField.value = "";
-});
+	let editButton = document.createElement('button')
+	editButton.classList.add('btn', 'edit')
+	editButton.innerText = 'Edit'
+	todoItem.appendChild(editButton)
+
+	let deleteButton = document.createElement('button')
+	deleteButton.classList.add('btn', 'delete')
+	deleteButton.innerText = 'Delete'
+	todoItem.appendChild(deleteButton)
+
+	deleteButton.addEventListener('click', () =>
+		todoContainer.removeChild(todoItem)
+	)
+
+	doneButton.addEventListener('click', () => {
+		todoText.classList.add('line-through')
+		editButton.setAttribute('disabled', 'disabled')
+	})
+
+	editButton.addEventListener('click', () => {
+		inputField.value = todoText.innerText
+		todoContainer.removeChild(todoItem)
+	})
+
+	inputField.value = ''
+}
+
+document.querySelector('.add').addEventListener('click', add)
