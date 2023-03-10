4eee4b14be98b00b1182de62a0f77c67667896b0
bosvill <sv.boecker@gmail.com>
2023-03-10 13:20:23 +0100

    with working storage

--- app.js
+++ app.js
@@ -1,48 +1,110 @@
-// 1. Create variables
-const todoContainer = document.querySelector('.to-dos')
-const todoItem = document.querySelectorAll('.todo-item')
-const inputField = document.querySelector('#inputField')
+// Global local storage declaration
+// let todos = [] 
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || []
+	const addNewTodo = document.querySelector('.form')
 
-const add = () => {
-	let todoItem = document.createElement('li')
-	todoItem.classList.add('todo-item')
-	todoContainer.appendChild(todoItem)
+	addNewTodo.addEventListener('submit', e => {
+		e.preventDefault()
+		console.log(e.target)
+		let todo = {
+			// createdAt: new Date().getTime(),
+			inputField: e.target.elements.inputField.value,
+			done: false
+		}
+		console.log(todo.inputField)
+		todos.push(todo)
 
-	let todoText = document.createElement('p')
-	todoText.classList.add('todo-text')
-	todoText.innerText = inputField.value
-	todoItem.appendChild(todoText)
+		localStorage.setItem('todos', JSON.stringify(todos))
 
-	let doneButton = document.createElement('button')
-	doneButton.classList.add('btn', 'done')
-	doneButton.innerText = 'Done'
-	todoItem.appendChild(doneButton)
+		e.target.reset()
 
-	let editButton = document.createElement('button')
-	editButton.classList.add('btn', 'edit')
-	editButton.innerText = 'Edit'
-	todoItem.appendChild(editButton)
+		createTodoItems()
+	})
+	createTodoItems()
+})
 
-	let deleteButton = document.createElement('button')
-	deleteButton.classList.add('btn', 'delete')
-	deleteButton.innerText = 'Delete'
-	todoItem.appendChild(deleteButton)
+function createTodoItems () {
+	const todoContainer = document.querySelector('.to-dos')
+	todoContainer.innerHTML = ""
 
-	deleteButton.addEventListener('click', () =>
-		todoContainer.removeChild(todoItem)
-	)
+	todos.forEach(todo => {
+		let todoItem = document.createElement('li')
+		todoItem.classList.add('todo-item')
+		todoContainer.appendChild(todoItem)
 
-	doneButton.addEventListener('click', () => {
+		let todoText = document.createElement('p')
+		todoText.classList.add('todo-text')
+		todoText.innerText = `${todo.inputField}`
+		todoItem.appendChild(todoText)
+
+		let doneButton = document.createElement('input')
+		doneButton.classList.add('btn','done')  
+		doneButton.type ='checkbox'
+		doneButton.label = 'Done'
+		doneButton.checked = todo.done
+		doneButton.innerText = 'Done'
+		todoItem.appendChild(doneButton)
+
+		let editButton = document.createElement('button')
+		editButton.classList.add('btn', 'edit')
+		editButton.innerText = 'Edit'
+		todoItem.appendChild(editButton)
+
+		let deleteButton = document.createElement('button')
+		deleteButton.classList.add('btn', 'delete')
+		deleteButton.innerText = 'Delete'
+		todoItem.appendChild(deleteButton)
+
+		if(todo.done) {
+			todoText.classList.add('line-through')
+		}
+
+		doneButton.addEventListener('click', () => {
 		todoText.classList.toggle('line-through')
-		// editButton.setAttribute('disabled', 'disabled')
-	})
 
-	editButton.addEventListener('click', () => {
-		inputField.value = todoText.innerText
-		todoContainer.removeChild(todoItem)
-	})
+		})
 
-	inputField.value = ''
+		doneButton.addEventListener('change', (e) => {
+			todo.done = e.target.checked
+			localStorage.setItem('todos', JSON.stringify(todos))
+
+			/* if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+ */
+			createTodoItems()
+		})
+
+		// todoText.addEventListener('dclick', (e) =>{
+		// 	todo.done = e.target.valueof(`!value`)
+		// 	localStorage.setItem('todos', JSON.stringify(todos))
+
+		// 	createTodoItems()
+
+		// })
+
+		editButton.addEventListener('click', () => {
+			inputField.value = todoText.innerText
+			todoContainer.removeChild(todoItem)
+			todos = todos.filter(t => t != todo)
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			createTodoItems()
+		})
+	
+		deleteButton.addEventListener('click', () => {
+			todos = todos.filter(t => t != todo)
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			createTodoItems()
+		})
+		
+})
 }
 
-document.querySelector('.add').addEventListener('click', add)
+
+
+
